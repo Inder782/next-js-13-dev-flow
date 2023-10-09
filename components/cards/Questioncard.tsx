fd7e5face7e0af697eddb6595ea0f75a74d7ecdd
inder782 <inderchandel782@gmail.com>
2023-10-09 13:48:32 +0530

    fix:4

--- components/cards/Questioncard.tsx
+++ components/cards/Questioncard.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 import React from "react";
 import RenderTag from "../shared/RenderTag";
 import Metric from "../shared/Metric";
-import { formatNumber, getTimestamp } from "@/lib/utils";
+import { formatAndDivideNumber, getTimestamp } from "@/lib/utils";
 
 interface QuestionProps {
   _id: string;
@@ -21,7 +21,8 @@ interface QuestionProps {
   answers: Array<object>;
   createdAt: Date;
 }
-const Questioncard = ({
+
+const QuestionCard = ({
   _id,
   title,
   tags,
@@ -32,53 +33,58 @@ const Questioncard = ({
   createdAt,
 }: QuestionProps) => {
   return (
-    <div className="card-wrapper p-9 sm:px-11 rounded-[10px]">
+    <div className="card-wrapper rounded-[10px] p-9 sm:px-11">
       <div className="flex flex-col-reverse items-start justify-between gap-5 sm:flex-row">
         <div>
           <span className="subtle-regular text-dark400_light700 line-clamp-1 flex sm:hidden">
             {getTimestamp(createdAt)}
           </span>
           <Link href={`/question/${_id}`}>
-            <h3 className="sem:h3-semibold base-semibold text-dark200_light900 line-clamp-2 flex-1">
+            <h3 className="sm:h3-semibold base-semibold text-dark200_light900 line-clamp-1 flex-1">
               {title}
             </h3>
           </Link>
         </div>
-        {/* if signed in add edit delete actions*/}
+
+        {/* If signed in add edit delete actions */}
       </div>
+
       <div className="mt-3.5 flex flex-wrap gap-2">
         {tags.map((tag) => (
           <RenderTag key={tag._id} _id={tag._id} name={tag.name} />
         ))}
       </div>
+
       <div className="flex-between mt-6 w-full flex-wrap gap-3">
         <Metric
-          imgUrl="/assets/icons/avatar.svg"
+          imgUrl={author.picture}
           alt="user"
-          title={`asked ${getTimestamp(createdAt)}`}
           value={author.name}
+          title={` - asked ${getTimestamp(createdAt)}`}
           href={`/profile/${author._id}`}
-          textStyles="body-medium text-dark400_light800"
+          isAuthor
+          textStyles="body-medium text-dark400_light700"
         />
+
         <Metric
           imgUrl="/assets/icons/like.svg"
-          alt="upvotes"
-          title=" Upvotes"
-          value={formatNumber(upvotes)}
+          alt="Upvotes"
+          value={formatAndDivideNumber(upvotes)}
+          title=" Votes"
           textStyles="small-medium text-dark400_light800"
         />
         <Metric
           imgUrl="/assets/icons/message.svg"
           alt="message"
-          title=" Message"
-          value={formatNumber(answers.length)}
+          value={formatAndDivideNumber(answers.length)}
+          title=" Answers"
           textStyles="small-medium text-dark400_light800"
         />
         <Metric
           imgUrl="/assets/icons/eye.svg"
-          alt="views"
+          alt="eye"
+          value={formatAndDivideNumber(views)}
           title=" Views"
-          value={formatNumber(views)}
           textStyles="small-medium text-dark400_light800"
         />
       </div>
@@ -86,4 +92,4 @@ const Questioncard = ({
   );
 };
 
-export default Questioncard;
+export default QuestionCard;
